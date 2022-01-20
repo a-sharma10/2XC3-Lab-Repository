f4ce5dcaf96143384d82bbc730a832e97c11f9d9
a-sharma10 <sharmaabhi2002@hotmail.com>
2022-01-19 22:57:26 -0500

    Added code to code.py

---
+++ code.py
@@ -0,0 +1,23 @@
+def are_valid_groups(studentNumbers, groups):
+
+    studentNumbersAux = []
+
+    for i in range(0, len(studentNumbers)):
+
+        studentNumbersAux.append(False)
+
+    for i in range(0, len(studentNumbers)):
+
+        for j in range(0, len(groups)):
+
+            if (studentNumbers[i] in groups[j]):
+
+                studentNumbersAux[i] = True
+
+    if (False in studentNumbersAux):
+
+        return False
+
+    else:
+
+        return True
